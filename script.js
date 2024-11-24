86ca2734d873bd9e9e527db64bb7571891ae8391
Gampesh <gampesh@gmail.com>
2024-11-24 20:26:52 +0530

    Create script.js for vastu

---
+++ script.js
@@ -0,0 +1,14 @@
+// Basic form validation
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if (name && email && message) {
+        alert("Thank you for contacting us, " + name + ". We will get back to you soon!");
+    } else {
+        alert("Please fill out all fields.");
+    }
+});
